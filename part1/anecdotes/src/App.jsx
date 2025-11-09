a83a53eacce51f90fb9ee66ee5a98bd78b010307
Kitsune7X <ryuizanagi@proton.me>
2025-11-09 18:12:17 +0900

    add voting and random selection to anecdotes

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { use, useState } from "react";
 
 const App = () => {
   const anecdotes = [
@@ -12,23 +12,59 @@ const App = () => {
     "The only way to go fast, is to go well.",
   ];
 
+  // Initialize an array filled with 0 that has the same
+  // length as the anecdotes array length
+  const votes = Array(anecdotes.length).fill(0);
+
+  const [selected, setSelected] = useState(0);
+
+  const [vote, setVote] = useState(votes);
+
   // Generate random index and filter it
 
   // ---------- Create random index function ----------
   const createRandomIndex = (arr) => Math.floor(Math.random() * arr.length);
 
-  // ---------- Filter out anecdotes from index ----------
-  const createAnecdotes = (arr) => {
-    // Create random index then find the item with that index
-    const idx = createRandomIndex(arr);
-    return arr.find((item) => arr.indexOf(item) === idx);
+  // ---------- Set random anecdotes ----------
+  const setRandomAnecdotes = (arr) => setSelected(createRandomIndex(arr));
+
+  // https://react.dev/reference/react/useState#updating-objects-and-arrays-in-state
+
+  // ---------- Update vote count function ----------
+  const updateVote = () => {
+    // Create a copy of the vote state
+    const copy = [...vote];
+
+    // Since `selected` state indicate the index of the current item in the array,
+    // we update the value of the item at that index in the array
+    copy[selected] += 1;
+
+    setVote(copy);
   };
-  // console.log(createRandomIndex(anecdotes));
-  // console.log(createAnecdotes(anecdotes));
 
-  const [selected, setSelected] = useState(0);
+  // Find the max vote value with Math.max()
+  console.log(Math.max(...vote));
 
-  return <div>{anecdotes[selected]}</div>;
+  // Find the index of the max value
+  console.log(vote.findIndex((item) => item === Math.max(...vote)));
+
+  const findHighestVoteIndex = (arr) =>
+    arr.findIndex((item) => item === Math.max(...arr));
+
+  return (
+    <div>
+      <Header text="Anecdote of the day" />
+      <AnecdotesDisplay item={anecdotes[selected]} />
+      <p>has {vote[selected]} votes</p>
+      <Button
+        onClick={() => setRandomAnecdotes(anecdotes)}
+        text="next anecdotes"
+      />
+      <Button onClick={() => updateVote()} text="Vote" />
+      <Header text="Anecdote with most votes" />
+      <p>Highest vote index {findHighestVoteIndex(vote)}</p>
+    </div>
+  );
 };
 
 export default App;
@@ -36,3 +72,26 @@ export default App;
 // -------------------------------------------------
 // Components
 // -------------------------------------------------
+// ---------- Anecdotes display ----------
+const AnecdotesDisplay = ({ item }) => <p>{item}</p>;
+
+// ---------- Vote display ----------
+const VotesDisplay = ({ item }) => <p>{item}</p>;
+
+// ---------- Header ----------
+const Header = ({ text }) => <h2>{text}</h2>;
+
+// ---------- Button ----------
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
+
+// -------------------------------------------------
+// Optional function
+// -------------------------------------------------
+// The function below will return the array with random index directly
+
+// // ---------- Filter out anecdotes from index ----------
+// const createRandomAnecdotes = (arr) => {
+//   // Create random index then find the item with that index
+//   const idx = createRandomIndex(arr);
+//   return arr.find((item) => arr.indexOf(item) === idx);
+// };
