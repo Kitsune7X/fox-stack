6ad2f730347bf9aad0261a56e33ef3cc16239949
Kitsune7X <ryuizanagi@proton.me>
2025-11-14 21:37:34 +0900

    clean up code

--- part2/country-info/src/App.jsx
+++ part2/country-info/src/App.jsx
@@ -3,7 +3,6 @@ import axios from "axios";
 
 const App = () => {
   const apiKey = import.meta.env.VITE_API_KEY;
-  // console.log(apiKey);
 
   const [country, setCountry] = useState([]);
   const [newCountry, setNewCountry] = useState("");
@@ -23,23 +22,9 @@ const App = () => {
     axios
       .get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
       .then((response) => {
-        // console.log(response.data);
         setCountry(response.data);
       });
   }, []);
-
-  // useEffect(() => {
-  //   if (!perfectMatch) return;
-  //   axios
-  //     .get(
-  //       `https://studies.cs.helsinki.fi/restcountries/api/name/${perfectMatch}`
-  //     )
-  //     .then((response) => {
-  //       console.log(response.data);
-  //       setFilter(filter.concat(response.data));
-  //       // console.log(response.data.name.common);
-  //     });
-  // }, [perfectMatch]);
 
   useEffect(() => {
     if (!perfectMatch) return;
@@ -48,12 +33,9 @@ const App = () => {
       const { data } = await axios.get(
         `https://api.openweathermap.org/data/2.5/weather?q=${perfectMatch}&appid=${apiKey}`
       );
-      console.log(data);
 
       setWeather(data);
       setWeatherIcon(data.weather.reduce((acc, curr) => acc + curr.icon, ""));
-
-      // console.log(weather.weather.icon);
     };
     fetchData();
   }, [perfectMatch]);
@@ -63,44 +45,20 @@ const App = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("Here!");
   };
 
   // ---------- Search country function ----------
   const search = (term, list) => {
-    // if (!newCountry) return;
-    // const perfectCell = list.find(
-    //   (item) => term.trim().toLowerCase() === item.name.common.toLowerCase()
-    // );
-    // console.log(perfectCell);
-
-    // if (perfectCell) {
-    //   console.log("WTF");
-    //   setPerfectMatch(term.trim().toLowerCase());
-    //   // setFilter([]);
-    //   setFilter(perfectCell);
-
-    //   return perfectCell;
-    // } else {
-    // setPerfectMatch("");
-
     const x = list.filter((item) => {
       const re = new RegExp(term.trim().toLowerCase());
       return item.name.common.toLowerCase().search(re) !== -1;
     });
-    // console.log(x);
     if (x.length === 1)
       setPerfectMatch(x.reduce((acc, curr) => acc + curr.name.common, ""));
     else setPerfectMatch("");
     setFilter(x);
-    // }
   };
 
-  // const c = search(newCountry, country);
-  // console.log(`c: ${c}`);
-  console.log(perfectMatch);
-
-  // console.log(handleSearchResult(c));
   // ==============================
   // * Function — END
   // ==============================
@@ -146,6 +104,7 @@ const App = () => {
                     src={`https://openweathermap.org/img/wn/${weatherIcon}@2x.png`}
                     alt="weather condition image"
                   />
+                  <p>Wind: {weather?.wind?.speed} m/s</p>
                 </div>
               </div>
             ))
@@ -174,78 +133,8 @@ const App = () => {
           ""
         )}
       </div>
-
-      {/* <p>
-        <span>debug </span>
-        {country.name.common}
-      </p> */}
-      {/* <button
-        onClick={() => {
-          setPerfectMatch(`japan`);
-        }}
-      >
-        debug button
-      </button> */}
-      {/* <div>
-        {perfectMatch &&
-          filter.map((item) => (
-            <div key={item.name}>
-              <h1>{item.name.common}</h1>
-              <p>Capital: {item.capital}</p>
-              <p>Area: {item.area}</p>
-              <h2>Languages</h2>
-              <ul>
-                {Object.entries(item.languages).map(([_, lang]) => (
-                  <li key={_}>{lang}</li>
-                ))}
-              </ul>
-              <div>
-                <img src={item.flags.svg} alt={item.flags.alt} width={150} />
-              </div>
-            </div>
-          ))}
-      </div> */}
-
-      {/* Weather testing  */}
     </div>
   );
 };
 
 export default App;
-
-// const languages = {
-//   fra: "French",
-//   gsw: "Swiss German",
-//   ita: "Italian",
-//   roh: "Romansh",
-// };
-// console.log([...languages]);
-
-// console.log(Array.from(languages));
-// const arr = Object.entries(languages);
-// console.log(arr);
-// const m = arr.map(([_, k]) => {
-//   return k;
-// });
-
-// console.log(m);
-
-// Add button to each list first. When the button is clicked, set perfectMatch status
-
-// console.log("" + "test string");
-
-// const as = [{ a: "he he", b: "WTF" }];
-
-// const lb = as.reduce((acc, curr) => acc + curr.a, "");
-// console.log(lb);
-
-{
-  /* <div>
-  <h2>Weather in {item.capital}</h2>
-  <img
-    src={`https://openweathermap.org/img/wn/${weather.weather.icon}@2x.png`}
-    alt="wtf"
-  />
-  <p>Temperature: {weather.main.temp}</p>
-</div>; */
-}
