d355a913b0810840247d3c6f723f4fe891a3289d
Kitsune7X <ryuizanagi@proton.me>
2025-11-16 05:59:53 +0900

    complete 3.3

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -26,12 +26,38 @@ let persons = [
 
 // Test endpoint
 app.get('/', (request, response) => {
-  response.send('<h1>WTF!</h1>');
+  response.send('<h1>Hello!</h1>');
 });
 
 // Get data
 app.get('/api/persons', (request, response) => {
   response.json(persons);
+});
+
+// Showing info
+app.get('/info', (request, response) => {
+  const timestamp = new Date().toString();
+  const count = persons.length;
+  //   console.log(count);
+
+  response.send(`
+                <p>Phone book has info of ${count} people</p>
+                <p>${timestamp}</p>
+                `);
+});
+
+// Display the information for a single phone book entry
+// If an entry for the given id is not found, the server
+//  has to respond with the appropriate status code.
+// Get the correct route first
+app.get('/api/persons/:id', (request, response) => {
+  //https://expressjs.com/en/5x/api.html#req.params
+  console.log(`id: ${id}`);
+  const person = persons.find((z) => z.id === id);
+
+  if (!person)
+    return response.status(404).json({ error: 'NOT FOUND' });
+  else response.json(person);
 });
 
 const PORT = 3001;
---
+++ part3/phonebook-backend/sandbox.js
@@ -0,0 +1,9 @@
+const d = Date.now();
+console.log(d);
+
+console.log(new Date());
+const time = new Date().getTime();
+console.log(time);
+
+const z = new Date().toString();
+console.log(z);
